5cb80693fa625281df19a2adb440a4ff0d2424d4
Anton Shestakov <prradd@gmail.com>
2021-06-22 10:05:44 +0300

    calculate

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -16,6 +16,36 @@ const Tables = () => {
 
 
     useEffect(() => {
+        let tmpSum: IVehiclesResult  = {
+            vehicleName: "",
+            pilots: [],
+            homePlanets: [],
+            homePlanetsSum: 0
+        };
+
+        const calcMaxSum: (vehicle: IVehicle) => void = (vehicle) => {
+            console.log("blah");
+            let sum = 0;
+            const pilots = vehicle.pilots;
+            for (let i = 0; i < pilots.length; i++) {
+                const homeWorldUrl = pilotsMap.get(pilots[i])?.homeworld;
+                let homeWorldPop = homeWorldUrl ? planetsMap.get(homeWorldUrl)?.population : 0;
+
+                if (typeof homeWorldPop === "string") {
+                    homeWorldPop = homeWorldPop ? parseInt(homeWorldPop) : 0
+                }
+                sum += homeWorldPop ? homeWorldPop : 0;
+            }
+            if (sum > tmpSum.homePlanetsSum) {
+                tmpSum = {
+                    vehicleName: vehicle.name,
+                    pilots: pilots,
+                    homePlanets: [],
+                    homePlanetsSum: sum
+                }
+            }
+        }
+
         const vehiclesMap = new Map<string, IVehicle>();
         const pilotsMap = new Map<string, IPeople>();
         const planetsMap = new Map<string, IPlanet>();
@@ -36,39 +66,24 @@ const Tables = () => {
                 await mapPlanets([value.homeworld], planetsMap);
             }
 
-        })().then(() => {
             // Calculate highest sum of "supporting" population
             for (const value of Array.from(vehiclesMap.values())) {
-                console.log("value");
-                console.log(value);
-                let sum = 0;
-                const pilots = value.pilots;
-                for (let i = 0; i < pilots.length; i++) {
-                    const homeWorldUrl = pilotsMap.get(pilots[i])?.homeworld;
-                    let homeWorldPop = homeWorldUrl ? planetsMap.get(homeWorldUrl)?.population : 0;
-
-                    if (typeof homeWorldPop === "string") {
-                        homeWorldPop = homeWorldPop ? parseInt(homeWorldPop) : 0
-                    }
-                    sum += homeWorldPop ? homeWorldPop : 0;
-                }
-                if (sum > supVehicle.homePlanetsSum) {
-                    setSupVehicle({
-                        vehicleName: value.name,
-                        pilots: pilots,
-                        homePlanets: [],
-                        homePlanetsSum: sum
-                    })
-                }
+                await calcMaxSum(value);
             }
+
+            console.log(tmpSum);
+            setSupVehicle(tmpSum);
             setIsLoaded(true);
-            console.log(supVehicle);
 
-        });
+        })();
 
 
-    }, [supVehicle])
-    console.log(supVehicle);
+    }, [])
+
+    // useEffect(() => {
+    //
+    // })
+        console.log(supVehicle);
 
     if (!isLoaded) {
         return <div>Loading...</div>;
